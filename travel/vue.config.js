7e30d1979d28a7721f02f208059e764c121eeaf7
1521350289@qq.com <1521350289@qq.com>
2019-04-07 14:56:50 +0800

    ajax city

--- travel/vue.config.js
+++ travel/vue.config.js
@@ -1,9 +1,13 @@
-const goods = require('./public/mock/index.json')
+const index = require('./public/mock/index.json')
+const city = require('./public/mock/city.json')
 module.exports = {
   devServer: {
     before: (app) => {
       app.get('/api/index.json', function (req, res) {
-        res.json(goods)
+        res.json(index)
+      })
+      app.get('/api/city.json', function (req, res) {
+        res.json(city)
       })
     }
   }
